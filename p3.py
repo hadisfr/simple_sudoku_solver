d80f7100987d0a21ab472dd75f6e171f9b993454
h@di <hadisafari76@gmail.com>
2017-05-03 23:14:31 +0430

    debug backtrack

--- p3.py
+++ p3.py
@@ -17,37 +17,41 @@ def complete_validate(table, l, i):
 				conflict_list.add(get_index((square[0] + j, square[1] + k), l))
 	return ((len(conflict_list) > 0), conflict_list)
 
-def solve(table, domains, l, i = 0):
+def solve(table, domains, primary_list, l, i = 0):
 	if i >= len(table):
 		return (True, set())
 
 	if table[i] != 0:
-		return solve(table, domains, l, i + 1)
+		return solve(table, domains, primary_list, l, i + 1)
 
 	conflict_list = set()
 	for j in domains[i]:
 		table[i] = j
 		(had_conflict, local_conflict_list) = complete_validate(table, l, i)
 		if not had_conflict:
-			(child_is_ok, child_conflict_list) = solve(table, domains, l, i + 1)
+			(child_is_ok, child_conflict_list) = solve(table, domains, primary_list, l, i + 1)
 			if child_is_ok:
 				return (True, set())
 			else:
-				if i not in child_conflict_list:
-					# print(i, table[i], conflict_list, "Child CF: ", child_conflict_list)
+				# print(i, table[i], conflict_list, "Child CF: ", child_conflict_list, child_conflict_list.intersection(primary_list), " :")
+				if len(child_conflict_list) > 0 and len(child_conflict_list.intersection(primary_list)) == 0 and i not in child_conflict_list:
+					# print(i, table[i], conflict_list, "Child CF: ", child_conflict_list, child_conflict_list.intersection(primary_list))
 					# print_table(table, l, " ", "_")
 					table[i] = 0
 					return (False, child_conflict_list)
 		else:
 			conflict_list = conflict_list.union(local_conflict_list)
-	# print(i, table[i], conflict_list)
+	# print(i, table[i], "(%r)" % domains[i], conflict_list)
 	# print_table(table, l, " ", "_")
 	table[i] = 0
 	return (False, conflict_list)
 
 def make_domains(table, l):
 	domains = []
+	primary_list = set()
 	for i in range(len(table)):
+		if table[i] == 0:
+			primary_list.add(i)
 		domains.append(set())
 		for j in range(l * l):
 			domains[i].add(j + 1)
@@ -72,15 +76,15 @@ def make_domains(table, l):
 
 	order_list.sort(key=lambda x: x[1])
 
-	return (domains, [x[0] for x in order_list])
+	return (domains, [x[0] for x in order_list], primary_list)
 
 if __name__ == '__main__':
 	(table, l) = get_input()
 
 	print_table(table, l, " ", "_")
 	print("")
-	(domains, order_list) = make_domains(table, l)
-	if solve(table, domains, l)[0]:
+	(domains, order_list, primary_list) = make_domains(table, l)
+	if solve(table, domains, primary_list, l)[0]:
 		print_table(table, l, " ", "_")
 	else:
 		print("Could not been filled.")
